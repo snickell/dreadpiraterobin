84fdc14274e89b04f3bd2cd06d2920759416f68b
Seth Nickell <snickell@gmail.com>
2020-11-12 21:43:10 -1000

    MAKE AWESOME CHANGES Protect brownie honor.

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,23 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
+  <div>Hi Sath!</div>
+  <PersonHater name="Sath" />
+  <PersonHater name="Rob" />
+  <PersonHater name="Elon Musk" />
+  <PersonHater name="Brownie" />
   <HelloWorld msg="Hello Vue 3 in CodeSandbox!" />
+  <div>Hi Sath!</div>
+  <PersonHater name="People with mental disabilities" />
 </template>
 
 <script>
 import HelloWorldVue from "./components/HelloWorld.vue";
+import PersonHater from "./components/PersonHater.vue";
 export default {
   name: "App",
   components: {
     HelloWorld: HelloWorldVue,
+    PersonHater: PersonHater,
   },
 };
 </script>
@@ -21,5 +30,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+div {
+  color: limegreen;
 }
 </style>
---
+++ src/components/PersonHater.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    Spew hate about {{ name }}
+    <div>
+      <button v-on:click="doHate" type="button">Hate on {{ name }}</button>
+      (You have invoked {{ howManyHates }} hates on {{ name }})
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PersonHater",
+  props: {
+    name: String,
+  },
+  data() {
+    return {
+      howManyHates: 0,
+    };
+  },
+  methods: {
+    doHate() {
+      if (this.name === "Brownie") {
+        alert("FUCK YOU: those who hate brownie hate themselves");
+        this.howManyHates = -9000;
+      } else {
+        alert("FUCK " + this.name);
+        this.howManyHates = this.howManyHates + 1;
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+div {
+  padding: 20px;
+  background: black;
+  color: red;
+}
+</style>
